b1aeb5fa671b2aa79bac07896ceb327fe6575ea2
Amedee Van Gasse <amedee@vangasse.eu>
2012-01-29 22:40:10 +0100

    connect to twitter asynchronously and show the timeline

--- src/main/java/be/amedee/twitterscripts/App.java
+++ src/main/java/be/amedee/twitterscripts/App.java
@@ -1,13 +1,40 @@
 package be.amedee.twitterscripts;
 
+import twitter4j.AsyncTwitter;
+import twitter4j.AsyncTwitterFactory;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.TwitterAdapter;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	static final Object LOCK = new Object();
+
+	public static void main(String[] args) throws InterruptedException {
+		AsyncTwitter twitter = new AsyncTwitterFactory().getInstance();
+		twitter.addListener(new TwitterAdapter() {
+
+			public void gotHomeTimeline(ResponseList<Status> statuses) {
+				System.out.println("Showing friends timeline.");
+				for (Status status : statuses) {
+					System.out.println(status.getUser().getName() + ":"
+							+ status.getText());
+
+				}
+				synchronized (LOCK) {
+					LOCK.notify();
+				}
+			}
+
+		});
+
+		twitter.getHomeTimeline();
+		synchronized (LOCK) {
+			LOCK.wait();
+		}
+	}
 }
